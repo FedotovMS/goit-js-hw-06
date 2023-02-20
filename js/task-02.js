5702ae594a3a077133c07690cc7b058e660aa4c5
Mykola Fedotov <fedotovns@gmail.com>
2023-02-20 17:41:04 +0100

    task-02 - done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsList = ingredients.forEach(function (item, index) {
+  const newEl = document.createElement("li");
+  newEl.textContent = item;
+  newEl.classList.add("item");
+  document.querySelector("#ingredients").append(newEl);
+});
