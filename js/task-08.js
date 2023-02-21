3be695b1819acedbc36529d3002251bd7a33f01f
Mykola Fedotov <fedotovns@gmail.com>
2023-02-21 18:19:19 +0100

    task-07 - done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const formObject = Object.fromEntries(formData.entries());
+
+  if (!formObject.email || !formObject.password) {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  console.log(formObject);
+
+  form.reset();
+}
